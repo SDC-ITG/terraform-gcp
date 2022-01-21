8d362082125791fb22989aeae5d72ac2e1ac3a8d
SDC-ITG <96460151+SDC-ITG@users.noreply.github.com>
2022-01-21 16:37:38 +0900

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@
 ## Provider
 ##----------------------------------------------------------------------
 provider "google" {
-  credentials = ${{ secrets.GOOGLE_CREDENTIALS }}" #jsonファイルの格納先を記載
+  credentials = ${{ secrets.GOOGLE_CREDENTIALS }} #jsonファイルの格納先を記載
   project     = "clear-healer-265405" #プロジェクトIDを記載
   region      = "asia-northeast1"
   zone        = "asia-northeast1-a"

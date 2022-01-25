a6dce1542a2d352089ba0fbc8183b9d732240da3
SDC-ITG <96460151+SDC-ITG@users.noreply.github.com>
2022-01-25 16:07:26 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,5 +12,9 @@ resource "google_compute_instance" "instance1" {
       image = "debian-cloud/debian-10"
     }
   }
+  network_interface {
+    network    = google_compute_network.vpc_network.name
+    subnetwork = google_compute_subnetwork.pub_subnet.name
+    access_config {}
+  }
 }
-#test

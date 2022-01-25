cadb2ba89c05a8fdc11035692ef2dc6c65d80599
SDC-ITG <96460151+SDC-ITG@users.noreply.github.com>
2022-01-25 15:43:32 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,26 +12,4 @@ resource "google_compute_instance" "instance1" {
       image = "debian-cloud/debian-10"
     }
   }
-  network_interface {
-    network    = google_compute_network.vpc.name
-    subnetwork = google_compute_subnetwork.pub_subnet.name
-    access_config {}
-  }
-}
-
-# Compute Engine
-resource "google_compute_instance" "instance2" {
-  name         = "terraform-test2"
-  machine_type = "e2-micro"
-  zone         = "asia-northeast1-a"
-  boot_disk {
-    initialize_params {
-      image = "windows-cloud/windows-2012-r2"
-    }
-  }
-  network_interface {
-    network    = google_compute_network.vpc.name
-    subnetwork = google_compute_subnetwork.pub_subnet.name
-    access_config {}
-  }
 }

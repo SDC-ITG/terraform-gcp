0669f9a48da592ddc2cb039b1750fef81a787c04
SDC-ITG <96460151+SDC-ITG@users.noreply.github.com>
2022-01-31 14:59:31 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@
 ##----------------------------------------------------------------------
 
 # Compute Engine
-resource "google_compute_instance" "instance2" {
-  name         = "terraform-test2"
+resource "google_compute_instance" "instance1" {
+  name         = "terraform-test1"
   machine_type = "e2-micro"
   zone         = "asia-northeast1-a"
   boot_disk {
@@ -12,9 +12,9 @@ resource "google_compute_instance" "instance2" {
       image = "debian-cloud/debian-10"
     }
   }
- ##- network_interface {
- ##-   network    = google_compute_network.vpc_network.name
- ##-   subnetwork = google_compute_subnetwork.pub_subnet.name
- ##-   access_config {}
- ##- }
+  network_interface {
+   network    = google_compute_network.vpc_network.name
+    subnetwork = google_compute_subnetwork.pub_subnet.name
+    access_config {}
+  }
 }

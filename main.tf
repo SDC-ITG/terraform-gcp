3b98eef04622eab22fa09ebb9ac794c88c198231
SDC-ITG <96460151+SDC-ITG@users.noreply.github.com>
2022-02-04 11:44:21 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@
 
 
 resource "google_compute_instance" "instance1" {
-  name         = "terraform-test2"
+  name         = "terraform-test1"
   machine_type = "e2-micro"
   zone         = "asia-northeast1-a"
   boot_disk {
